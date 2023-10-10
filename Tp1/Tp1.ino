46d33c6eef2071eebaaca7be622be0553a6907c6
Thomas QUENTEL <thquentel@local.isima.fr>
2023-10-10 09:51:17 +0200

    Tp2 q1-2-3-4

--- Tp1/Tp1.ino
+++ Tp1/Tp1.ino
@@ -1,16 +1,8 @@
-/*
-  Lora Send And Receive
-  This sketch demonstrates how to send and receive data with the MKR WAN 1300/1310 LoRa module.
-  This example code is in the public domain.
-*/
-
-#include <MKRWAN.h>
 #include "Wire.h"
 #include "SHT31.h"
-#define SHT31_ADDRESS   0x44
+#include <Arduino.h>
 
-SHT31 sht;
-uint16_t status;
+#include <MKRWAN.h>
 
 LoRaModem modem;
 
@@ -47,10 +39,59 @@ void setup() {
   // NOTE: independent of this setting, the modem will
   // not allow sending more than one message every 2 minutes,
   // this is enforced by firmware and can not be changed.
+}
 
-  
-  Serial.println("Setup Capteur");
+void loop() {
+  Serial.println();
+  Serial.println("Enter a message to send to network");
+  Serial.println("(make sure that end-of-line 'NL' is enabled)");
 
+  while (!Serial.available());
+  String msg = Serial.readStringUntil('\n');
+
+  Serial.println();
+  Serial.print("Sending: " + msg + " - ");
+  for (unsigned int i = 0; i < msg.length(); i++) {
+    Serial.print(msg[i] >> 4, HEX);
+    Serial.print(msg[i] & 0xF, HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  int err;
+  modem.beginPacket();
+  modem.print(msg);
+  err = modem.endPacket(true);
+  if (err > 0) {
+    Serial.println("Message sent correctly!");
+  } else {
+    Serial.println("Error sending message :(");
+    Serial.println("(you may send a limited amount of messages per minute, depending on the signal strength");
+    Serial.println("it may vary from 1 message every couple of seconds to 1 message every minute)");
+  }
+  delay(1000);
+  if (!modem.available()) {
+    Serial.println("No downlink message received at this time.");
+    return;
+  }
+  char rcv[64];
+  int i = 0;
+  while (modem.available()) {
+    rcv[i++] = (char)modem.read();
+  }
+  Serial.print("Received: ");
+  for (unsigned int j = 0; j < i; j++) {
+    Serial.print(rcv[j] >> 4, HEX);
+    Serial.print(rcv[j] & 0xF, HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+
+/*
+void setup()
+{
   Serial.begin(115200);
   Serial.println(__FILE__);
   Serial.print("SHT31_LIB_VERSION: \t");
@@ -67,41 +108,26 @@ void setup() {
 
   sht.heatOn();
 
+  while (sht.isHeaterOn())
+  {
+    status = sht.readStatus();
+    printHeaterStatus(status);
+    sht.read();
+    Serial.print("Temperature :");
+    Serial.println(sht.getTemperature());
+    delay(1000);
+  }
 
-  Serial.println("End Setup !");
+  Serial.println("switched off");
 }
 
-void loop() {
-  /*
-  status = sht.readStatus();
-  printHeaterStatus(status);
-  */
 
-  int8_t datas[10];
-  for (int i=0; i<10; i++)
-   {
-      sht.read();
-      datas[i] = (int8_t)sht.getTemperature();
-      delay(3000);
-   }
-
-  int err;
-  modem.beginPacket();
-  modem.write((uint8_t*)datas, 10);
-  err = modem.endPacket(true);
-  if (err > 0) {
-    Serial.println("Message sent correctly!");
-  } else {
-    Serial.println("Error sending message :(");
-    Serial.println("(you may send a limited amount of messages per minute, depending on the signal strength");
-    Serial.println("it may vary from 1 message every couple of seconds to 1 message every minute)");
-  }
-  delay(1000);
-  if (!modem.available()) {
-    Serial.println("No downlink message received at this time.");
-    return;
-  }
+void loop()
+{
+  // forced switch off
+  if (status & SHT31_STATUS_HEATER_ON) sht.heatOff();
 }
+
 
 void printHeaterStatus(uint16_t status)
 {
@@ -114,3 +140,4 @@ void printHeaterStatus(uint16_t status)
     Serial.println("OFF");
   }
 }
+*/
